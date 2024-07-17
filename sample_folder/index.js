5805190f7474d210b979a25151884ebe76bb48b0
KngCd <panaligankingcedrick@gmail.com>
2024-07-17 11:01:53 +0800

    Modified my codes

--- sample_folder/index.js
+++ sample_folder/index.js
@@ -1,1 +1,23 @@
-/* Counter */
+/* SIMPLE COUNTER */
+
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const displayCount = document.getElementById("display");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count ++;
+    displayCount.textContent = count;
+}
+
+decreaseBtn.onclick = function () {
+    count --;
+    displayCount.textContent = count;
+};
+
+resetBtn.onclick = function () {
+    count = 0;
+    displayCount.textContent = count;
+}
