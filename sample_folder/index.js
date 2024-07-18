da941350e07e65e8b98c09a462ace89c7f49b430
KngCd <panaligankingcedrick@gmail.com>
2024-07-18 10:57:25 +0800

    Add some changes

--- sample_folder/index.js
+++ sample_folder/index.js
@@ -1,5 +1,17 @@
 /* SIMPLE COUNTER */
 
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color;
+    do {
+        color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+    } while (color === '#FFFFFF')
+    return color;
+}
+
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const decreaseBtn = document.getElementById("decreaseBtn");
@@ -10,14 +22,20 @@ let count = 0;
 increaseBtn.onclick = function(){
     count ++;
     displayCount.textContent = count;
+    displayCount.style.color = getRandomColor();
+    displayCount.style.backgroundColor = getRandomColor();
 }
 
 decreaseBtn.onclick = function () {
     count --;
     displayCount.textContent = count;
-};
+    displayCount.style.color = getRandomColor();
+    displayCount.style.backgroundColor = getRandomColor();
+}
 
 resetBtn.onclick = function () {
     count = 0;
     displayCount.textContent = count;
+    displayCount.style.color = getRandomColor();
+    displayCount.style.backgroundColor = "#FFFFFF";
 }
